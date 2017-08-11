0e4ff330ce975cc18f808055d35b116388f08093
Christina L. Cole <christinalcole@gmail.com>
2017-08-10 23:15:39 -0400

    reset initial specs based on revised schema

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,22 +1,5 @@
 require 'rails_helper'
 
 Rspec.describe Position, type: :model do
-  before(:each) do
-    @user = User.first
-    @crew = Crew.create
-    @position = Position.create(name: "main trim", user_id: @user.id, crew_id: @crew.id)
-  end
-
-  it "is has a name" do
-    expect(@position.name).to_not be(nil)
-  end
-
-  xit "has and belongs to many users" do
-    expect()
-  end
-
-  xit "has and belongs to many crews" do
-
-  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,5 @@
 require 'rails_helper'
 
 Rspec.describe User, type: :model do
-  before(:each) do
-    @user = User.first
-    @crew = Crew.create
-    @position = Position.create(name: "main trim", user_id: @user.id, crew_id: @crew.id)
-  end
-
-  it "is valid with a name, email address, and password" do
-    expect(user).to be_valid
-  end
-
-  it "is not valid without a password" do
-    expect(User.new(name: "Ahab", email: "snowhill@whitewhale.com")).not_to be_valid
-  end
-
-  it "is not valid without an email address" do
-    expect(User.new(name: "Ahab", password: "thar-blow$")).not_to be_valid
-  end
-
-  it "has many positions" do
-    expect(@user.positions).to include(@position)
-  end
-
-  it "has many crews through positions" do
-    expect(@user.crews).to include(@crew)
-  end
+  
 end
