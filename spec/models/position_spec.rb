b5c6c2511ed75644823e19f85767df53704395fe
Christina L. Cole <christinalcole@gmail.com>
2017-08-13 23:03:52 -0400

    update Position model validation specs

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Position, type: :model do
   describe 'validations' do
-    it 'has a valid factory'
-    it 'is not valid without a name'
+    it 'has a valid factory' do
+      expect(build(:position)).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      position = build(:position, name: nil)
+
+      expect(position).not_to be_valid
+      expect(position.errors.full_messages).to eq(["Name can't be blank"])
+    end
   end
 
   describe 'associations' do
