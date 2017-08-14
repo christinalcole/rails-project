dd84c6c81a2deaa2f1d5c348c0e11749e5370600
Christina L. Cole <christinalcole@gmail.com>
2017-08-13 22:09:52 -0400

    update User model validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,40 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it 'has a valid factory'
-    it 'is invalid without a first_name'
-    it 'is invalid without a last_name'
-    it 'is invalid without an email'
-    it 'is invalid without a phone number'
-    it 'is invalid without a weight'
+    it 'has a valid factory' do
+      expect(build(:user)).to be_valid
+    end
+
+    it 'is invalid without a first_name' do
+      user = build(:user, first_name: nil)
+
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to eq("First_name can't be blank")
+    end
+    it 'is invalid without a last_name' do
+      user = build(:user, last_name: nil)
+
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to eq("Last_name can't be blank")
+    end
+    it 'is invalid without an email' do
+      user = build(:user, email: nil)
+
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to eq("Email can't be blank")
+    end
+    it 'is invalid without a phone number' do
+      user = build(:user, phone_number: nil)
+
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to eq("Phone_number can't be blank")
+    end
+    it 'is invalid without a weight' do
+      user = build(:user, weight: nil)
+
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to eq("Weight can't be blank")
+    end
   end
 
   describe 'associations' do
