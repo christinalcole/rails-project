37bb32f8e207fd410d2567de71e19f1594f58c77
Christina L. Cole <christinalcole@gmail.com>
2017-08-13 22:29:16 -0400

    correct error message format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,31 +10,31 @@ RSpec.describe User, type: :model do
       user = build(:user, first_name: nil)
 
       expect(user).not_to be_valid
-      expect(user.errors.full_messages).to eq("First name can't be blank")
+      expect(user.errors.full_messages).to eq(["First name can't be blank"])
     end
     it 'is invalid without a last_name' do
       user = build(:user, last_name: nil)
 
       expect(user).not_to be_valid
-      expect(user.errors.full_messages).to eq("Last name can't be blank")
+      expect(user.errors.full_messages).to eq(["Last name can't be blank"])
     end
     it 'is invalid without an email' do
       user = build(:user, email: nil)
 
       expect(user).not_to be_valid
-      expect(user.errors.full_messages).to eq("Email can't be blank")
+      expect(user.errors.full_messages).to eq(["Email can't be blank"])
     end
     it 'is invalid without a phone number' do
       user = build(:user, phone_number: nil)
 
       expect(user).not_to be_valid
-      expect(user.errors.full_messages).to eq("Phone number can't be blank")
+      expect(user.errors.full_messages).to eq(["Phone number can't be blank"])
     end
     it 'is invalid without a weight' do
       user = build(:user, weight: nil)
 
       expect(user).not_to be_valid
-      expect(user.errors.full_messages).to eq("Weight can't be blank")
+      expect(user.errors.full_messages).to eq(["Weight can't be blank"])
     end
   end
 
