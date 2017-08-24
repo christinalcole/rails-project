88fa9a2b7bcdad90d24b2772e2807e68ae0cf6f9
Christina L. Cole <christinalcole@gmail.com>
2017-08-24 00:04:35 -0400

    Revise sign-in, sign up specs for case-insensitivity
    Devise began humanizing the authentication_keys variable referenced in its default locale file in version 4.1.0.  Setting specs to look for case-insensitive error messages that would contain "Email" based on authentication_key.

--- spec/features/users/signin_and_signup_spec.rb
+++ spec/features/users/signin_and_signup_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Sign in', :devise do
   scenario 'a user cannot sign in if not registered' do
     signin('test@example.com', 'please123')
-    expect(page).to have_content I18n.t 'devise.failure.not_found_in_database', authentication_keys: 'email'
+    expect(page).to have_content (/#{I18n.t('devise.failure.not_found_in_database', authentication_keys: 'email')}/i)
   end
 
   scenario 'a user can sign in with valid credentials' do
@@ -15,13 +15,13 @@ RSpec.feature 'Sign in', :devise do
   scenario 'a user cannot sign in with a wrong email' do
     user = FactoryGirl.create(:user)
     signin('invalid@email.com', user.password)
-    expect(page).to have_content I18n.t 'devise.failure.not_found_in_database', authentication_keys: 'email'
+    expect(page).to have_content (/#{I18n.t('devise.failure.not_found_in_database', authentication_keys: 'email')}/i)
   end
 
   scenario 'a user cannot sign in with wrong password' do
     user = FactoryGirl.create(:user)
     signin(user.email, 'invalidpass')
-    expect(page).to have_content I18n.t 'devise.failure.invalid', authentication_keys: 'email'
+    expect(page).to have_content (/#{I18n.t('devise.failure.not_found_in_database', authentication_keys: 'email')}/i)
   end
 end
 
