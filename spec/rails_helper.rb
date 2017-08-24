7f20ef553fbfd63f70840c9856b0627686febf6b
Christina L. Cole <christinalcole@gmail.com>
2017-08-23 21:52:55 -0400

    correct testing support for Devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'support/helpers/session_helpers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -58,4 +59,8 @@ RSpec.configure do |config|
 
   # support for factory_girl_rails
   config.include FactoryGirl::Syntax::Methods
+
+  #support for Devise
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Features::SessionHelpers, type: :feature
 end
