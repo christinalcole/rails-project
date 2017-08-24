abb2bc43f1519d27fbf8dd0d2450011fd6521502
Christina L. Cole <christinalcole@gmail.com>
2017-08-23 21:19:35 -0400

    add initial spec for sign in, sign up

---
+++ spec/features/users/signin_and_signup_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature 'Sign in', type: :devise do
+  scenario 'a user cannot sign in if not registered' do
+    sign_in('test@example.com', 'please123')
+    expect(page).to have_content I18n.t 'devise.failure.not_found_in_database', authentication_keys: 'email'
+  end
+
+  scenario 'a user can sign in with valid credentials' do
+    user = FactoryGirl.create(:user)
+    sign_in(user.email, user.password)
+    expect(page).to have_content I18n.t 'devise.sessions.signed_in'
+  end
+
+  scenario 'a user cannot sign in with a wrong email' do
+    user = FactoryGirl.create(:user)
+    sign_in('invalid@email.com', user.password)
+    expect(page).to have_content I18n.t 'devise.failure.not_found_in_database', authentication_keys: 'email'
+  end
+
+  scenario 'a user cannot sign in with wrong password' do
+    user = FactoryGirl.create(:user)
+    sign_in(user.email, 'invalidpass')
+    expect(page).to have_content I18n.t 'devise.failure.invalid', authentication_keys: 'email'
+  end
+end
+
+RSpec.feature 'Sign up', type: :devise do
+  scenario 'a user cannot sign up if already registerd'
+  scenario 'a user can sign up with valid credentials'
+  scenario 'a user cannot sign up without an email'
+  scenario 'a user cannot sign up without a unique email'
+  scenario 'a user cannot sign up without a valid password'
+end
