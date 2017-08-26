c00e9e5f199a951049f8c8c9e828c73a6294bb24
Christina L. Cole <christinalcole@gmail.com>
2017-08-26 15:46:26 -0400

    allow user signup to pass model parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
 
   def index
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :weight])
   end
 end
