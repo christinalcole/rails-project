9c6cd29c147b9626c40bd0cf9805e1f7941bd3ca
Christina L. Cole <christinalcole@gmail.com>
2018-03-17 18:14:03 -0400

    initial specs for adding positions to user profile

--- spec/features/position_features_spec.rb
+++ spec/features/position_features_spec.rb
@@ -10,6 +10,40 @@ RSpec.feature 'Users and Positions', type: :feature do
     end
   end
 
+  context 'user wants to add positions to their profile' do
+    scenario 'there is a link to add new positions' do
+      user = create(:user)
+      signin(user.email, user.password)
+
+      visit user_positions_path(user.id)
+      expect(page).to have_link("Add Positions")
+    end
+
+    scenario 'there is a form to add new positions to the current user profile' do
+      user = create(:user)
+      signin(user.email, user.password)
+
+      visit user_positions_path(user.id)
+      click_link("Add Positions")
+      expect(page).to have_css("form#new_user_#{user.id}")
+    end
+
+    scenario 'the form should display the existing positions in the db' do
+      position1 = create(:position)
+      position2 = create(:position)
+      user = create(:user)
+
+      signin(user.email, user.password)
+      visit new_user_position_path(user.id)
+
+      expect(page).to have_content(position1.name)
+      expect(page).to have_content(position2.name)
+    end
+
+    scenario 'the form should allow entry of a skill level for each position'
+    scenario 'the positions in the current user profile should change after form submission'
+  end
+
   context 'user wants to manage positions on their profile' do
     scenario 'there is a link to manage positions' do
       user = create(:user)
