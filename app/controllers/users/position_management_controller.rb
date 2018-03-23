bb2b2bbb9e66dffee8fdacf0cf1ab3e5ac7850ef
Christina L. Cole <christinalcole@gmail.com>
2018-03-23 15:38:58 -0400

    Revise Form to Give Fully Nested Params
    Changed form approach in order to (finally!) get desired params
    structure:
    
    {... "_method"=>"patch",... "user"=>{"positions_users_attributes"=>
    {"0"=>{"position_id"=>"1", "skill_level"=>"4"}, "1"=>{"skill_level"
    =>""}}}, "commit"=>"Update User", "controller"=>"users", "action"=>
    "update", "id"=>"1"}
    
    Revised Controller to set instance variables for Position.all and
    current_user.positions_users.  Form loops through all positions,
    using position id attribute to build params hash for PositionsUser
    object that would include a skill level attribute if the position
    is selected by the user in the form.
    
    Should be able to work with this params hash, start writing to
    the PositionsUser table!

--- app/controllers/users/position_management_controller.rb
+++ app/controllers/users/position_management_controller.rb
@@ -1,20 +1,32 @@
 class Users::PositionManagementController < ApplicationController
+  # def edit
+  #   positions_users = current_user.positions_users
+  #   Position.find_each do |position|
+  #     unless positions_users.detect {|pu| pu.position_id == position.id}
+  #       current_user.positions_users.build(position_id: position.id)#, skill_level: 0)
+  #     end
+  #   end
+  # end
 
   def edit
-    positions_users = current_user.positions_users
-    Position.find_each do |position|
-      unless positions_users.detect {|pu| pu.position_id == position.id}
-        current_user.positions_users.build(position_id: position.id)
-      end
-    end
+    @positions = Position.all
+    @positions_users = current_user.positions_users
   end
 
   def update
     raise params.inspect
-    if current_user.update(user_params)
+    # binding.pry
+    if current_user.update(user_position_params)
       redirect_to user_positions_path(current_user)
     else
       render 'edit'
     end
   end
+
+  private
+  def user_position_params
+    params.require(:user).permit(:user_id, :position_ids[], positions_users_attributes: [:skill_level])
+  end
 end
+
+# <ActionController::Parameters {"utf8"=>"✓", "_method"=>"patch", "authenticity_token"=>"0DGcIiju/zzDHvfNK1bfvUwJ+BU91Kn4a22HpR/qjPdzoPlSCwHfREBZpHmGonQG4iIgaIegwiXMQ/RRZoY24Q==", "user"=>{"positions_users"=>[{"position_id"=>"", "skill_level"=>""}, {"position_id"=>""}, {"position_id"=>"2", "skill_level"=>"1"}]}, "commit"=>"Manage Position(s)", "controller"=>"users/position_management", "action"=>"update", "user_id"=>"1"} permitted: false>
